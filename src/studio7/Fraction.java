fee7bb50b8f11b5997f20b2f7d0e71982a367740
jessicalee <jessicalee@localhost>
2024-10-30 15:52:28 -0500

    done

--- src/studio7/Fraction.java
+++ src/studio7/Fraction.java
@@ -1,10 +1,28 @@
 package studio7;
 
 public class Fraction {
+	private double numerator;
+	private double denominator;
+	
+	public Fraction(int num, int denom) {
+		numerator = num;
+		denominator = denom;
+		
+	}
+	
+	public FractionSum(int num1, int denom1, int num2, int denom2) {
+		numerator = num1 * denom2 + num2 * denom1;
+		denominator = denom1 * denom2;
+		Fraction(numerator, denominator);
+		
+		crossMultiply = numerator / denominator ;
+		return Fraction();
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		FractionSum(1,2,1,4);
+		
 	}
 
 }
